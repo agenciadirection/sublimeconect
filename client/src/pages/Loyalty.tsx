7107d57a68e08e70e09b3fcc813ebe446bdf1a7e
agenciadirection <agenciadirection@gmail.com>
2025-12-03 00:35:17 -0300

    Delete client/src/pages directory

--- client/src/pages/Loyalty.tsx
+++
@@ -1,9 +0,0 @@
-// Sistema de Fidelidade Avançado Integrado - Sublime Connect
-// Migrado do sistema PHP/SQLite para React/TypeScript/PostgreSQL
-// Desenvolvido com todas as funcionalidades avançadas do sistema original
-
-import LoyaltyAdvanced from './LoyaltyAdvanced';
-
-export default function Loyalty() {
-  return <LoyaltyAdvanced />;
-}
